29604798a3296e5b3aab6abd078c4decb6e3bfb9
Manuel Gil <manugildev@gmail.com>
2021-09-28 00:49:26 +0100

    Implement TVL calculator

---
+++ src/reserve.rs
@@ -0,0 +1,11 @@
+use super::*;
+use solana_program::{
+    clock::Slot,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    program_pack::{IsInitialized, Pack, Sealed},
+    pubkey::{Pubkey, PUBKEY_BYTES},
+};
+
+/// Lending market reserve state
